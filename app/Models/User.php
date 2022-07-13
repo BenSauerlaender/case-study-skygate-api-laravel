18f42fb0695e4f17a2adf5efe3e2551fc99096ba
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-07-13 10:44:14 +0200

    add relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,28 @@
 
 namespace App;
 
+use App\Models\EmailChangeRequest;
+use App\Models\RefreshToken;
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
     use HasFactory;
+
+    public function role()
+    {
+        return $this->hasOne(Role::class);
+    }
+
+    public function refreshToken()
+    {
+        return $this->hasOne(RefreshToken::class);
+    }
+
+    public function emailChangeRequest()
+    {
+        return $this->hasOne(EmailChangeRequest::class);
+    }
 }
